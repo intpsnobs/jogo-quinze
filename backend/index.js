1fb73883b364cb7c53d4c1441c445b7ae6214b2a
Peter Brendel <peter_brendel@hotmail.com>
2020-09-21 22:58:11 -0300

    Tentando fazer um front

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
+const path = require('path');
 const express = require("express") // our express server
 const app = express() // generate an app object
 const cors = require("cors")
@@ -9,10 +10,13 @@ const PORT = process.env.PORT || 3000 // port that the server is running on => l
 
 app.use(express.json()) // telling the app that we are going to use json to handle incoming payload
 app.use(cors());
-
+app.get('/', async (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 app.use(router);
 
 app.listen(PORT, () => {
   // listening on port 3000
   console.log(`listening on port ${PORT}`) // print this when the server starts
 });
+
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('../routes/user');
+const gameRoutes = require('../routes/game');
 
 router.use(userRoutes);
+router.use(gameRoutes);
 
 module.exports =  router;
